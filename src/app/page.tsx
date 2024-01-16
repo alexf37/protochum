9604dc5c8f2d1ecf12861bf622d86110cb0b4ef7
Alex Foster <xrk4np@virginia.edu>
2024-01-16 00:37:22 -0500

    finish home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -45,6 +45,26 @@ function WrenchIcon(props: React.SVGProps<SVGSVGElement>) {
   );
 }
 
+function LinkAwayIcon(props: React.SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="currentColor"
+      className="h-6 w-6"
+      {...props}
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M13.5 6H5.25A2.25 2.25 0 0 0 3 8.25v10.5A2.25 2.25 0 0 0 5.25 21h10.5A2.25 2.25 0 0 0 18 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25"
+      />
+    </svg>
+  );
+}
+
 export default async function Home() {
   return (
     <>
@@ -54,11 +74,11 @@ export default async function Home() {
       </h1>
       <div className="flex gap-4">
         <Link
-          href="admin"
+          href="#calendly"
           className="group flex items-center gap-2 rounded-full border-2 border-indigo-400 bg-indigo-500 px-5 py-3 text-xl font-semibold drop-shadow"
         >
-          Admin
-          <WrenchIcon className="h-5 w-5 transition-all group-hover:-rotate-12" />
+          Calendly
+          <LinkAwayIcon className="h-5 w-5 transition-all group-hover:-translate-y-0.5" />
         </Link>
         <Link
           href="survey"
@@ -68,6 +88,12 @@ export default async function Home() {
           <LeftArrowIcon className="h-5 w-5 transition-all group-hover:translate-x-1" />
         </Link>
       </div>
+      <Link
+        href="admin"
+        className=" group fixed bottom-2 left-2 flex aspect-square items-center gap-2 rounded-full border-2 border-indigo-400 bg-indigo-500 p-3 text-xl font-semibold drop-shadow"
+      >
+        <WrenchIcon className="h-5 w-5 transition-all group-hover:-rotate-12" />
+      </Link>
     </>
   );
 }
