0720506774b0d1ad76ddcf54505de4045dc0a7b5
Alex Foster <xrk4np@virginia.edu>
2024-01-18 14:00:53 -0500

    make background animate

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={`font-sans ${inter.variable}`}>
         <TRPCReactProvider cookies={cookies().toString()}>
-          <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#c8d3f7] to-[#bfaffc] text-white">
+          <main className="gradient-animated flex min-h-screen flex-col items-center justify-center text-white">
             <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
               {children}
             </div>
