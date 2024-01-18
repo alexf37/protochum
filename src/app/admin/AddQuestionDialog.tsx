c0a0e59a3f5b1f6743f22dfa766a079b0f05834e
Alex Foster <xrk4np@virginia.edu>
2024-01-18 01:02:30 -0500

    form-ify mcq form

--- src/app/admin/AddQuestionDialog.tsx
+++ src/app/admin/AddQuestionDialog.tsx
@@ -22,6 +22,21 @@ import {
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
 
 const colours = [
   "Red",
@@ -42,12 +57,17 @@ const formSchema = z.object({
       message: "Option must be at least 1 character.",
     }),
   ),
-  includeOther: z.boolean(),
+  includeOther: z.enum(["yes", "no"]),
 });
 
 function McqForm() {
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
+    defaultValues: {
+      prompt: "",
+      options: [""],
+      includeOther: "no",
+    },
   });
   function onSubmit(values: z.infer<typeof formSchema>) {
     // Do something with the form values.
@@ -55,61 +75,123 @@ function McqForm() {
     console.log(values);
   }
   return (
-    <form
-      onSubmit={(e) => {
-        e.preventDefault();
-      }}
-    >
-      <div className="grid gap-4 py-2">
-        <div className="grid grid-cols-4 items-center gap-4">
-          <Label htmlFor="name" className="text-right">
-            Prompt
-          </Label>
-          <Input
-            id="prompt"
-            placeholder="What's your favorite color?"
-            className="col-span-3"
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)}>
+        <div className="grid py-2">
+          <FormField
+            control={form.control}
+            name="prompt"
+            render={({ field }) => (
+              <FormItem className="grid grid-cols-4 items-center gap-4">
+                <FormLabel className="text-right">Prompt</FormLabel>
+                <FormControl>
+                  <Input
+                    placeholder="What's your favorite color?"
+                    className="col-span-3"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
           />
-        </div>
-        <div className="row-auto grid grid-cols-4 items-center gap-4">
-          <Label htmlFor="option" className="text-right">
-            Option 1
-          </Label>
-          <Input
-            id="option"
-            placeholder={colours[Math.floor(Math.random() * colours.length)]}
-            className="col-span-3"
-          />
-        </div>
+          {form.getValues().options.map((_, i) => (
+            <FormField
+              key={i}
+              control={form.control}
+              name={`options.${i}`}
+              render={({ field }) => (
+                <FormItem className="grid grid-cols-4 items-center gap-4">
+                  <FormLabel className="text-right">Option {i + 1}</FormLabel>
+                  <FormControl>
+                    <div className="col-span-3 flex gap-1">
+                      <Input
+                        placeholder={
+                          colours[Math.floor(Math.random() * colours.length)]
+                        }
+                        className="col-span-8"
+                        {...field}
+                      />
+                      <TooltipProvider>
+                        <Tooltip>
+                          <TooltipTrigger asChild>
+                            <Button
+                              variant="outline"
+                              type="button"
+                              className="aspect-square text-red-600"
+                            >
+                              *
+                            </Button>
+                          </TooltipTrigger>
+                          <TooltipContent>
+                            <p>Require elaboration</p>
+                          </TooltipContent>
+                        </Tooltip>
+                        <Tooltip>
+                          <TooltipTrigger asChild>
+                            <Button
+                              variant="outline"
+                              type="button"
+                              className="aspect-square"
+                            >
+                              –
+                            </Button>
+                          </TooltipTrigger>
+                          <TooltipContent>
+                            <p>Remove option</p>
+                          </TooltipContent>
+                        </Tooltip>
+                      </TooltipProvider>
+                    </div>
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          ))}
 
-        <div className="row-auto grid grid-cols-4 items-center gap-4">
-          <Button
-            variant="outline"
-            type="button"
-            className="col-span-3 col-start-2 justify-start text-slate-400"
-          >
-            + Add option
-          </Button>
+          <div className="row-auto grid grid-cols-4 items-center gap-4 pt-2">
+            <Button
+              variant="outline"
+              type="button"
+              className="col-span-3 col-start-2 justify-start text-slate-400"
+              onClick={() => {
+                form.watch("options");
+                form.setValue("options", [...form.getValues().options, ""]);
+              }}
+            >
+              + Add option
+            </Button>
+          </div>
         </div>
-      </div>
-      <div className="row-auto grid grid-cols-4 items-center gap-4">
-        <Label htmlFor="other" className="text-right leading-4">
-          Include "Other"
-        </Label>
-        <Select>
-          <SelectTrigger className="col-span-3">
-            <SelectValue defaultValue={"no"} placeholder="❌ No (default)" />
-          </SelectTrigger>
-          <SelectContent>
-            <SelectItem value="yes">✅ Yes</SelectItem>
-            <SelectItem value="no">❌ No</SelectItem>
-          </SelectContent>
-        </Select>
-      </div>
-      <DialogFooter className="pt-4">
-        <Button type="submit">Save Question</Button>
-      </DialogFooter>
-    </form>
+        <FormField
+          control={form.control}
+          name="includeOther"
+          render={({ field }) => (
+            <FormItem className="row-auto grid grid-cols-4 items-center gap-4">
+              <FormLabel className="text-right leading-4">
+                Include "Other"
+              </FormLabel>
+              <Select onValueChange={field.onChange} defaultValue={field.value}>
+                <FormControl>
+                  <SelectTrigger className="col-span-3">
+                    <SelectValue />
+                  </SelectTrigger>
+                </FormControl>
+                <SelectContent>
+                  <SelectItem value="yes">✅ Yes</SelectItem>
+                  <SelectItem value="no">❌ No</SelectItem>
+                </SelectContent>
+              </Select>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <DialogFooter className="pt-4">
+          <Button type="submit">Save Question</Button>
+        </DialogFooter>
+      </form>
+    </Form>
   );
 }
 
